eb857f5bf5f23df477de659b7bbc9e8b57caefcc
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2022-02-10 10:06:34 -0400

    feat: getPokemon

---
+++ src/api/pokemon.js
@@ -0,0 +1,10 @@
+const axios = require('axios').default;
+
+axios.defaults.baseURL = 'https://pokeapi.co/api/v2/';
+
+
+module.exports.getPokemon = async (name) => {
+    const pokemon = await axios.get(`pokemon/${name}`);
+    return pokemon.data;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const pokemonApi = require('./api/pokemon');
 
 const port = 3000;
 
 app.use(cors({
-    origin:'*'
+    origin: '*'
 }));
 
-app.get('/',(req,res)=>{
-    console.log('Hello world');
-    res.sendStatus(200);
+app.get('/pokemon/:pokeName', async (req, res) => {
+    try {
+        const pokemon = await pokemonApi.getPokemon(req.params.pokeName);
+        res.status(200).send(pokemon)
+    } catch (error) {
+        res.sendStatus(error.response.status)
+    }
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Example listening port ${port}`);
 });
